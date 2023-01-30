b1b9747e9c9a1c57a10fece4c83e062ffad49a4a
Oche Ejeh <goodnessemmanuel@gmail.com>
2023-01-30 15:10:46 +0100

    add reset feature to counter

--- practice/counter/src/counter/Counter.jsx
+++ practice/counter/src/counter/Counter.jsx
@@ -11,6 +11,10 @@ import CounterButton from "./CounterButton";
 export default function Counter(){
     const [count, setCount] = useState(0);
 
+    function resetDisplay () {
+        console.log("Reset button clicked! value to clear: ", count )
+        setCount(0)
+    }
 
     return (
         <div className="component">
@@ -19,6 +23,9 @@ export default function Counter(){
             <CounterButton step={2} currentCount={count} callback={setCount}/>
             <CounterButton step={5} currentCount={count} callback={setCount}/>
             <CounterButton step={10} currentCount={count} callback={setCount}/>
+            <div className="reset">
+                <button className="btn-reset" type="reset" onClick={resetDisplay}> Reset </button>
+            </div>
         </div>
     )
 }
